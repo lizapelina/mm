388dff34c22549ced468285e2b356af340087348
Elizaveta <133259959+lizapelina@users.noreply.github.com>
2023-05-13 19:45:15 +0300

    Create py.py

---
+++ py.py
@@ -0,0 +1,8 @@
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+import random
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
